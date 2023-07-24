74ffcbbf9f73566d7f31f3063ca0321373f5791d
alijah <139933156+alijahanusha@users.noreply.github.com>
2023-07-24 11:34:49 -0400

    Add files via upload

--- alijah/MP2- Alijah/MP2- Alijah/ViewController3.swift
+++ alijah/MP2- Alijah/MP2- Alijah/ViewController3.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class ViewController3: UIViewController {
 
+    @IBOutlet weak var answerDisplay: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func response1(_ sender: UIButton) {
+        answerDisplay.text = "❌"
     }
-    */
-
+    
+    @IBAction func response2(_ sender: UIButton) {
+        answerDisplay.text = "❌"
+    }
+    
+    @IBAction func response3(_ sender: UIButton) {
+        answerDisplay.text = "✅"
+    }
+    
 }
